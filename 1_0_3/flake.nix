60ccdc61397c98c5e004ef1dee662b0a746cada6
hugosenari <hugosenari@gmail.com>
2022-03-27 21:51:26 -0300

    chore: re index libffi

--- 1_0_3/flake.nix
+++ 1_0_3/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-libffi-1_0_3.ref   = "refs/tags/1.0.3";
   inputs.src-libffi-1_0_3.owner = "Araq";
   inputs.src-libffi-1_0_3.repo  = "libffi";
-  inputs.src-libffi-1_0_3.dir   = "";
   inputs.src-libffi-1_0_3.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
--- 1_0_4/flake.nix
+++ 1_0_4/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-libffi-1_0_4.ref   = "refs/tags/1.0.4";
   inputs.src-libffi-1_0_4.owner = "Araq";
   inputs.src-libffi-1_0_4.repo  = "libffi";
-  inputs.src-libffi-1_0_4.dir   = "";
   inputs.src-libffi-1_0_4.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
